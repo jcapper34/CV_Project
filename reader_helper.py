35fceafe85e217d687e2533f2e4d61173c31bfad
John Capper <j.capper2@gmail.com>
2020-12-03 11:54:26 -0800

    More progress on alternate note detection

--- reader_helper.py
+++ reader_helper.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import math
 import cv2
 import os
@@ -213,13 +214,15 @@ def detect_notes(gray_img, staff): # NEEDS to take in 1 staff objects at a time,
 
 def detect_notes_1(binary_img, staff):
     C_THRESH = 0.6
+    vpadding = 0.5
 
     templates = [   # (Template Image, Counts)
         (cv2.imread(TEMPLATE_DIR+'/quarter-note.jpg'), 1),
         (cv2.imread(TEMPLATE_DIR+'/half-note-line.jpg'), 2),
         (cv2.imread(TEMPLATE_DIR+'/half-note.jpg'), 2),
     ]
-    vpadding = 0.5
+
+    notes = []  # ((x, y), counts)
     for template, counts in templates:
         # Convert Template to Binary
         template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
@@ -246,9 +249,25 @@ def detect_notes_1(binary_img, staff):
 
         match_image = np.copy(staff_image)
         for x, y in centroids[1::]:
+            cx = x + scaled_template.shape[0]/2
+            cy = y + scaled_template.shape[1]/2 + int(staff_top - vpadding * staff_height)
+            notes.append(((cx, cy), counts))
+
             x = int(x)
             y = int(y)
             cv2.rectangle(match_image, (x, y), (x + scaled_template.shape[1], y + scaled_template.shape[1]), (0, 0, 255), 2)
 
         cv2.imshow("%u Count Matches" % counts, match_image)
         cv2.waitKey(0)
+
+    notes.sort(key=lambda note: note[0][0])     # Sort notes by x value (left to right)
+
+
+    staff.set_notes([(coord[1], counts) for coord, counts in notes])
+
+    print(staff.notes)
+
+
+
+
+    pprint(notes)
