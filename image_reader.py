577fff0e633174677337aadcdbd6242351d6b311
Amira Ramirez <apramirezgonzalez@mymail.mines.edu>
2020-12-03 21:24:15 -0700

    Rotation accounted for, not beyond 90 deg, test images added

--- image_reader.py
+++ image_reader.py
@@ -4,7 +4,7 @@ import math
 
 
 def find_sheet_music(bgr_image):
-    B_THRESH = 100
+    B_THRESH = 150
 
     gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
 
@@ -13,7 +13,7 @@ def find_sheet_music(bgr_image):
 
     # Find contours
     contours, hierarchy = cv2.findContours(thresh_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #cv2.drawContours(image=bgr_image, contours=contours, color=(0, 0, 255), thickness=2, contourIdx=-1) TESTING ONLY
+    #cv2.drawContours(image=bgr_image, contours=contours, color=(0, 0, 255), thickness=2, contourIdx=-1) #TESTING ONLY
 
     # Sort contours by area
     contours = sorted(contours, key=cv2.contourArea)
@@ -23,6 +23,7 @@ def find_sheet_music(bgr_image):
     # Get the corner points of largest contour
     arclen = cv2.arcLength(contour, True)
     approx = cv2.approxPolyDP(contour, 0.02 * arclen, True)
+
 
     # Draw corner points TESTING ONLY
     # cv2.drawMarker(bgr_image, position=(approx[0][0][0], approx[0][0][1]), color=(255, 0, 0), #TL
@@ -34,18 +35,33 @@ def find_sheet_music(bgr_image):
     # cv2.drawMarker(bgr_image, position=(approx[3][0][0], approx[3][0][1]), color=(255, 0, 0), #TR
     #                markerType=cv2.MARKER_CROSS, thickness=50)
 
+    # Order corner points based on x-coord
+    corner_points = []
+    for i in range(4):
+        for j in range(len(corner_points)):
+            if(approx[i][0][0] < corner_points[j][0]):
+                temp = corner_points[j]
+                corner_points[j] = approx[i][0]
+                corner_points.append(temp)
+                break
+        else:
+            corner_points.append(approx[i][0])
+
     # Get new image dimensions based on corner points
-    x = abs(approx[3][0][0] - approx[0][0][0])
-    y = abs(approx[3][0][1] - approx[0][0][1])
+    x = abs(corner_points[1][0] - corner_points[0][0])
+    y = abs(corner_points[1][1] - corner_points[0][1])
     sheet_width = int(math.sqrt(pow(x, 2) + pow(y, 2)))
 
-    x = abs(approx[1][0][0] - approx[0][0][0])
-    y = abs(approx[1][0][1] - approx[0][0][1])
+    x = abs(corner_points[2][0] - corner_points[0][0])
+    y = abs(corner_points[2][1] - corner_points[0][1])
     sheet_height = int(math.sqrt(pow(x, 2) + pow(y, 2)))
 
     # Find homography
-    pts1 = np.array([approx[0][0], approx[1][0], approx[2][0], approx[3][0]]) # TL, BL, BR, TR
-    pts2 = np.array([[0, 0], [0, sheet_height], [sheet_width, sheet_height], [sheet_width, 0]])
+    pts1 = np.array([corner_points[0], corner_points[1], corner_points[2], corner_points[3]]) # Left to right on image (vary based on ration)
+    if (corner_points[1][1] < corner_points[0][1]):
+        pts2 = np.array([[0, sheet_height], [0, 0], [sheet_width, sheet_height], [sheet_width, 0]]) # BL TL BR TR
+    else:
+        pts2 = np.array([[0, 0], [0, sheet_height], [sheet_width, 0], [sheet_width, sheet_height]]) # TL BL TR BR
     H1, _ = cv2.findHomography(srcPoints=pts1, dstPoints=pts2)
 
     # Warp and get sheet music image
--- music_reader.py
+++ music_reader.py
@@ -7,12 +7,19 @@ from image_reader import *
 
 MEDIA_DIR = 'music-img'
 TEMPLATE_DIR = 'templates'
+REAL_IMG_DIR = 'real-pictures'
 
 
 def music_reader(filename):
     bgr_img = cv2.imread(filename)
+    bgr_img = find_sheet_music(bgr_img)   # COMMENT OUT to use music from MEDIA_DIR
+
     gray_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2GRAY)
-    _, binary_img = cv2.threshold(gray_img, 200, 255, cv2.THRESH_BINARY)
+    _, binary_img = cv2.threshold(gray_img, 150, 255, cv2.THRESH_BINARY)  # TODO: could adjust threshold, maybe figure out dynamic threshold approach
+
+    # cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+    # cv2.imshow("Image", binary_img)
+    # cv2.waitKey(0)
 
     staffs = detect_staff_lines(binary_img)
 
@@ -25,5 +32,6 @@ def music_reader(filename):
 
 
 if __name__ == '__main__':
-    filename = os.path.join(MEDIA_DIR, 'mary-had-a-little-lamb.jpg')
+    filename = os.path.join(REAL_IMG_DIR, 'twinkle-twinkle-little-star2.jpg')
+    #filename = os.path.join(MEDIA_DIR, 'mary-had-a-little-lamb.jpg')
     music_reader(filename)
